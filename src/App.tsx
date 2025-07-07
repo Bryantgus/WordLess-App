669e6598cd346955375de0afcbcefc0f99d14e37
Bryant Tejeda <bryanttejeda628f@hotmail.com>
2025-07-07 07:55:59 -0400

    commit 7

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import { Header } from "./components/Header";
 import { AllAttemptsWords } from "./components/AllAttemptsWords";
 import Keyboard from "./components/Keyboard";
-import { useCallback, useEffect, useRef, useState } from "react";
-import { generateAttempts, generateRandomWord, removeLastWord } from "./utils/logic";
+import { useEffect, useRef, useState } from "react";
+import { generateAttempts, generateRandomWord } from "./utils/logic";
 import { KeyPressedContext } from "./hook/KeyPressedContext";
-import { AttemptsContext } from "./hook/AttemptsContext";
 
+const LENGTH_RANDOM_WORD = 6
+const LENGTH_ATTEMPTS = 6
 type AttemptLetter = {
   letter: string
   isFill: boolean
@@ -16,17 +17,15 @@ type AttemptLetter = {
 export default function App() {
 
   const [key, setKey] = useState<string>('') //Global State listen onClick on key component
-  const [randomWord, setRandomWord] = useState<string>(generateRandomWord(6).toUpperCase())
-  const [attemptsLength, setAttemptsLength] = useState(6)
+  const [randomWord, setRandomWord] = useState<string>(generateRandomWord(LENGTH_RANDOM_WORD).toUpperCase())
+  const [attemptsLength, setAttemptsLength] = useState(LENGTH_ATTEMPTS)
   const [attempts, setAttempts] = useState<AttemptLetter[][]>([])
   const [attemptsPos, setAttemptsPos] = useState<number>(0)
   const PrevAttemptPos = useRef<number>(0)
-  const [myWord, setMyWord] = useState<string>('')
-  const prevMyWord = useRef<string>('')
+  const [myWord, setMyWord] = useState<string[]>([])
 
   useEffect(() => {
     console.log(randomWord);
-
   }, [randomWord]);
 
   //Generate Attempts
@@ -51,131 +50,55 @@ export default function App() {
     };
   }, []);
 
-  //Modifying keyPressed(button)
+  //Setting myWord
   useEffect(() => {
-
+    if (myWord.length >= randomWord.length) return
+    if (key === '') return
     if (key === 'Backspace') {
-      setMyWord(prev => removeLastWord(prev))
+      setMyWord(prev => prev.slice(0, -1))
     } else if (key === 'Enter') {
-
-      if (prevMyWord.current.length !== randomWord.length) return
-
-      setAttempts(prev => {
-        const currentAttempts = [...prev]
-        console.log(currentAttempts);
-
-        const attempModify: AttemptLetter[] = []
-        const myWordSplited: string[] = prevMyWord.current.split('')
-        const randomWordSplited: string[] = randomWord.split('')
-
-        myWordSplited.forEach((letter, index) => {
-          // console.log(randomWordSplited, 'random');
-          // console.log(myWordSplited, 'word');
-          // console.log(randomWordSplited.includes(letter))
-          // console.log(myWordSplited, myWordSplited);
-          // console.log(index);
-
-          // console.log(letter, randomWord[index]);
-
-          if (randomWordSplited.includes(letter)) {
-            if (myWordSplited[index] === randomWordSplited[index]) {
-              // console.log(1);
-              attempModify[index] = {
-                isCorrect: 1,
-                isFill: false,
-                wasAnswer: true,
-                letter: letter
-              }
-            } else {
-              // console.log(2);
-              attempModify[index] = {
-                isCorrect: 2,
-                isFill: false,
-                wasAnswer: true,
-                letter: letter
-              }
-            }
-          }
-          else {
-            // console.log(3);
-            attempModify[index] = {
-              isCorrect: 3,
-              isFill: false,
-              wasAnswer: true,
-              letter: letter
-            }
-          }
-          //Removing letter of the array
-          randomWordSplited[randomWordSplited.indexOf(letter)] = ' '
-          // console.log(attempModify);
-
-        });
-        currentAttempts[attemptsPos] = attempModify
-        return currentAttempts
-      })
-
-      setMyWord('')
+      console.log("Enter");
 
     } else if (/[a-zA-Z]/.test(key)) {
-      setMyWord(prev => {
-        if (prev.length < randomWord.length) {
-          prevMyWord.current = prev + key.toUpperCase()
-          return prev + key.toUpperCase()
-        } else {
-          return prev
-        }
-      })
+      setMyWord(prev => [...prev, key])
+      console.log(key);
+
     }
     setKey('')
-  }, [attemptsPos, key, randomWord])
+  }, [key, myWord.length, randomWord.length]);
 
-  // //Modifying current attempt
   useEffect(() => {
-
-    if (attemptsPos !== PrevAttemptPos.current) return
-
-
-    const letterIndex = myWord.length - 1
-    console.log(letterIndex, "Index");
-
-    const deleted = myWord.length < prevMyWord.current.length
-    console.log(deleted);
+    console.log(myWord);
 
     setAttempts(prev => {
-
-      //All attempts
-      const allAttempts = [...prev]
-
-      //Current attempt
-      const currentAttemptToModify = [...allAttempts[attemptsPos]]
-      // if (!currentAttemptToModify[letterIndex]) return allAttempts;
-
-      let currentLetterToModify: AttemptLetter
-
-      //Modifying properties of letter
-      if (!deleted) {
-        currentLetterToModify = { ...currentAttemptToModify[letterIndex] }
-        currentLetterToModify.letter = myWord.charAt(letterIndex)
-        currentLetterToModify.isFill = true
-      } else {
-        currentLetterToModify = { ...currentAttemptToModify[letterIndex + 1] }
-        currentLetterToModify.letter = ''
-        currentLetterToModify.isFill = false
+      const newAttempts = [...prev]
+      const modifyAttempt = myWord.map((letter: string) => {
+        return {
+          letter: letter,
+          isFill: true,
+          isCorrect: 3,
+          wasAnswer: true
+        }
+      })
+      while (modifyAttempt.length !== LENGTH_RANDOM_WORD) {
+        modifyAttempt.push({
+          letter: '',
+          isFill: false,
+          isCorrect: 0,
+          wasAnswer: false
+        })
       }
-
-      //Modifying current attempts with the new changes on letter
-      currentAttemptToModify[deleted ? letterIndex + 1 : letterIndex] = currentLetterToModify
-
-      //Pushing the attempt modified into its position
-      allAttempts[attemptsPos] = currentAttemptToModify
-
-      return allAttempts
+      newAttempts[attemptsPos] = modifyAttempt
+      // console.log(newAttempts);
+      
+      return newAttempts
     })
+  }, [attemptsPos, myWord]);
 
-    prevMyWord.current = myWord
+  useEffect(() => {
+    console.log(attempts, "attempts");
 
-  }, [myWord, randomWord.length, attemptsPos])
-
+  }, [attempts, myWord]);
   return (
     <div>
       <Header />
